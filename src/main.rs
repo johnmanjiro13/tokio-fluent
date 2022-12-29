ee6756480ee08cd2391478c7b4e0b6f73ad2f73b
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2022-12-29 21:57:43 +0900

    feat: Send message synchronistically

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::{collections::HashMap, time::SystemTime};
+
+use rmp_serde::Serializer;
+use serde::{Deserialize, Serialize};
+use tokio::io::{self, AsyncWriteExt};
+use tokio::net::TcpStream;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Record {
+    timestamp: u64,
+    entry: HashMap<String, String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Message {
+    tag: &'static str,
+    entries: Vec<Record>,
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut socket = TcpStream::connect("127.0.0.1:24224").await?;
+
+    let mut buf = Vec::new();
+    let mut map = HashMap::new();
+    map.insert("key".to_string(), "value".to_string());
+    let record = Record {
+        timestamp: SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+        entry: map,
+    };
+    let message = Message {
+        tag: "client.test",
+        entries: vec![record],
+    };
+    message.serialize(&mut Serializer::new(&mut buf)).unwrap();
+    socket.write_all(&buf).await?;
+
+    Ok(())
+}
