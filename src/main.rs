4c6cf773de433589dc51ea54f93abd4dee48aa1f
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2023-01-05 17:30:15 +0900

    feat: Define client struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use std::thread::sleep;
+use std::time::Duration;
 use std::{collections::HashMap, time::SystemTime};
 
+use crossbeam::channel::{self, Sender};
 use rmp_serde::Serializer;
 use serde::{Deserialize, Serialize};
 use tokio::io::{self, AsyncWriteExt};
 use tokio::net::TcpStream;
+use tokio::task::JoinHandle;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Record {
@@ -17,31 +21,65 @@ struct Message {
     entries: Vec<Record>,
 }
 
+struct Client {
+    sender: Sender<HashMap<String, String>>,
+    worker: JoinHandle<io::Result<()>>,
+}
+
+impl Client {
+    async fn new() -> io::Result<Client> {
+        let mut socket = TcpStream::connect("127.0.0.1:24224").await?;
+        let (sender, receiver) = channel::unbounded();
+
+        let worker = tokio::spawn(async move {
+            loop {
+                match receiver.try_recv() {
+                    Ok(entry) => {
+                        let record = Record {
+                            timestamp: SystemTime::now()
+                                .duration_since(SystemTime::UNIX_EPOCH)
+                                .unwrap()
+                                .as_secs(),
+                            entry,
+                        };
+                        let message = Message {
+                            tag: "client.test",
+                            entries: vec![record],
+                        };
+                        let mut buf = Vec::new();
+                        message.serialize(&mut Serializer::new(&mut buf)).unwrap();
+                        socket.write_all(&buf).await?;
+                    }
+                    Err(channel::TryRecvError::Empty) => continue,
+                    Err(channel::TryRecvError::Disconnected) => break,
+                }
+            }
+
+            Ok::<_, io::Error>(())
+        });
+
+        Ok(Client { sender, worker })
+    }
+
+    fn send(
+        &self,
+        entry: HashMap<String, String>,
+    ) -> Result<(), channel::SendError<HashMap<String, String>>> {
+        self.sender.send(entry)
+    }
+}
+
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let mut socket = TcpStream::connect("127.0.0.1:24224").await?;
+    let client = Client::new().await?;
+    let mut map = HashMap::new();
+    map.insert("Key".to_string(), "Value".to_string());
+    client.send(map).unwrap();
 
-    let send_task = tokio::spawn(async move {
-        let mut buf = Vec::new();
-        let mut map = HashMap::new();
-        map.insert("key".to_string(), "value".to_string());
-        let record = Record {
-            timestamp: SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs(),
-            entry: map,
-        };
-        let message = Message {
-            tag: "client.test",
-            entries: vec![record],
-        };
-        message.serialize(&mut Serializer::new(&mut buf)).unwrap();
-        socket.write_all(&buf).await?;
+    sleep(Duration::new(3, 0));
 
-        Ok::<_, io::Error>(())
-    });
-
-    send_task.await?;
+    let mut map2 = HashMap::new();
+    map2.insert("Key2".to_string(), "Value2".to_string());
+    client.send(map2).unwrap();
     Ok(())
 }
